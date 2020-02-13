340261e15f43501962126ffe66c5054062529004
Nour Boulahcen <nour.boulahcen@gmail.com>
2020-02-13 09:29:27 +0100

    first message deciphered

--- test.py
+++ test.py
@@ -3,7 +3,7 @@
 
 import os
 
-with open("D:/code/UE_Crypto_Charpak/Codes/message1.txt", 'r') as file:
+with open("./Codes/message1.txt", 'r') as file:
     # on fait des choses avec le fichier
     message = file.read() # chaîne de caractère avec le contenu du fichier
     # bla
@@ -21,13 +21,13 @@ def Scytale(code,p):
 def M1(code):
     for i in range(len(code)):
         dc = Scytale(code,i)
-        if "Joel" in dc:
-            f = open("D:/code/UE_Crypto_Charpak/message1_decrypted.txt", "a")
+        if "Joël" in dc:
+            f = open("message1_decrypted.txt", "a")
             f.write(dc + "\n\n\n-------------------------------------------------------------------------------------------------------------------\n\n\n")
             f.close()
             
-#M1(L)
-#print(Scytale(code,1447))
+M1(message)
+#print(Scytale(message,1447))
 class Transpose:
     """
     >>> Transpose(2)("abcd")
@@ -40,5 +40,5 @@ class Transpose:
         _out = "".join([_in[i::self.n] for i in range(self.n)])
         return _out
 
-f = Transpose(1447)
-print(f(message))
+#f = Transpose(1447)
+#print(f(message))
