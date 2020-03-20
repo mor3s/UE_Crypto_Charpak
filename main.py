0d53ef9c1044750f5c2b924b02e7b962308c1ff8
mor3s <nour.boulahcen@gmail.com>
2020-03-20 09:35:03 +0100

    add auto_deciphered function

--- main.py
+++ main.py
@@ -4,6 +4,23 @@ from lib import *
 from tqdm import tqdm
 
 
+
+def auto_decipher(code, n):
+    if max(freq(code).items(), key=operator.itemgetter(1))[0] == ' ':
+        method = scytale
+    else:
+        method = vigenere
+    print(method)
+    for i in tqdm(range(1, n)):
+        dc = method(code, i)
+        if "Joël" in dc :   # Choose a word wich could the most probably appear in the message
+            f = open("D:/code/UE_Crypto_Charpak/message_decrypted.txt", "a")
+            f.close()
+            print(dc)
+            break
+
+
+
 def decipher(code,method, n):
     for i in tqdm(range(1, n)):
         dc = method(code, i)
@@ -17,6 +34,8 @@ def decipher(code,method, n):
 #decipher(read("D:/code/UE_Crypto_Charpak/Codes/message1.txt"),scytale ,len(read("D:/code/UE_Crypto_Charpak/Codes/message1.txt")))
 #decipher(read("D:/code/UE_Crypto_Charpak/Codes/message7.txt"),vigenere, 100)
 
+#auto_decipher(read("D:/code/UE_Crypto_Charpak/Codes/message1.txt") ,len(read("D:/code/UE_Crypto_Charpak/Codes/message1.txt")))
+auto_decipher(read("D:/code/UE_Crypto_Charpak/Codes/message7.txt"), 100)
 
 
 
